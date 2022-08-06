d39dc3d1628f93e864ac317cb583c52e748f2fc2
IgorFreiria <igorfreiria.ti@gmail.com>
2022-08-06 18:38:27 -0300

    upando-arquivos

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Cliente igor = new Cliente();
+		igor.setNome("Igor");
+		
+		Conta cc = new ContaCorrente(igor);
+		Conta poupanca = new ContaPoupanca(igor);
+		
+		cc.depositar(100);
+		cc.transferir(100, poupanca);
+		
+		cc.imprimirExtrato();
+		poupanca.imprimirExtrato();
+	}
+
+}
